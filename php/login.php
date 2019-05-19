e09e791490fbe5dd2abc40aff97a16f7768585b7
skillerhero <48114839+skillerhero@users.noreply.github.com>
2019-05-19 12:07:40 -0500

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -4,9 +4,9 @@ $usuario=$_REQUEST['user'];
 $contrasena=$_REQUEST['pass'];
 
 if($usuario=='admin' and $contrasena=='admin'){
-	include '../html/menuAdm.html';
+	header('../html/menuAdm.html');
 }
 else{
-	include '../index.html';
+	header('../index.html');
 }
 ?>
