9b8c63640b497a22f60a6f4f237110043b1be4a1
skillerhero <48114839+skillerhero@users.noreply.github.com>
2019-05-19 17:48:08 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 
 <head>
-	<meta name="viewport" content="width=device-width">
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
 	<link rel='stylesheet' type='text/css' href='css/estilos.css' >
+	<link rel="shortcut icon" href="imagenes/favicon.png">
 	<title>Login</title>
 </head>
 <body style="background-image: url(imagenes/login.jpg);">
 
-			<h1>Bienvenido!</h1>
-			
-		<form name="formLogin" method="post" action="php/login.php"  style="background:rgba(255,255,255,0.4);">
-			<h2>Iniciar sesión</h2>
-			<br><input class="input" type="text" name="user" placeholder="&#128100;  Usuario" required autofocus>
-			<br><input type="password" name="pass" placeholder="&#9998;  Contraseña"required></br>
-			<br><input type ="submit" value="&#128275;  Entrar" class="botonEntrar"  />
-		</form>
+	<h1>Bienvenido!</h1>
+
+	<form name="formLogin" method="post" action="php/login.php"  style="background:rgba(255,255,255,0.4);">
+		<h2>Iniciar sesión</h2>
+		<br><input class="input" type="text" name="user" placeholder="&#128100;  Usuario" required autofocus>
+		<br><input type="password" name="pass" placeholder="&#9998;  Contraseña"required></br>
+		<br><input type ="submit" value="&#128275;  Entrar" class="botonEntrar"  />
+	</form>
 
 </body>
 </html>
