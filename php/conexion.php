e54c7d16e83dc60c8f889444dc53c704da629516
skillerhero <48114839+skillerhero@users.noreply.github.com>
2019-06-16 13:51:07 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,9 +1,9 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "basepw");
+$conexion = mysqli_connect("remotemysql.com", "VwyqdVFVEv", "DJRYGjlwt0", "VwyqdVFVEv");
 if (mysqli_connect_errno()) {
     printf("No se pudo establecer la conexion %s\n", mysqli_connect_error());
     exit();
 }
-$resultado=mysqli_select_db($conexion, "basepw");
+$resultado=mysqli_select_db($conexion, "VwyqdVFVEv");
 mysqli_set_charset($conexion, "utf8");
 ?>
