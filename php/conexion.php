b37ad5ef0daf1faf04082e5505faae6ab72db319
skillerhero <48114839+skillerhero@users.noreply.github.com>
2019-06-16 12:00:13 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("https://remotemysql.com/", "VwyqdVFVEv", "DJRYGjlwt0", "VwyqdVFVEv");
+$conexion = mysqli_connect("remotemysql.com", "VwyqdVFVEv", "DJRYGjlwt0", "VwyqdVFVEv");
 if (mysqli_connect_errno()) {
     printf("No se pudo establecer la conexion %s\n", mysqli_connect_error());
     exit();
